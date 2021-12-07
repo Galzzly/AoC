4a5cba1486f7758e6c8d8466b55acf11f1168549
Liam Gallear <liam.gallear@gmail.com>
2021-12-07 10:03:26 +0000

    testing out doing both in one function d7

--- 2021/D07/day07.go
+++ 2021/D07/day07.go
@@ -18,6 +18,10 @@ func main() {
 	t2 := time.Now()
 	fmt.Println("Part 2", part2(nums), '('+time.Since(t2)+')')
 	fmt.Println("Total time: ", time.Since(start))
+	fmt.Println()
+	tb := time.Now()
+	b1, b2 := both(nums)
+	fmt.Println("1:", b1, ", 2:", b2, "Took:", time.Since(tb))
 }
 
 func part1(nums []int) (res int) {
@@ -55,5 +59,24 @@ func part2(nums []int) (res int) {
 	}
 	sort.Ints(cost)
 	res = cost[0]
+	return
+}
+
+func both(nums []int) (r1, r2 int) {
+	sort.Ints(nums)
+	c1, c2 := []int{}, []int{}
+	for i := nums[0]; i <= nums[len(nums)-1]; i++ {
+		f1, f2 := 0, 0
+		for _, n := range nums {
+			steps := diff(i, n)
+			f1 += steps
+			f2 += (steps * (steps + 1)) / 2
+		}
+		c1 = append(c1, f1)
+		c2 = append(c2, f2)
+	}
+	sort.Ints(c1)
+	sort.Ints(c2)
+	r1, r2 = c1[0], c2[0]
 	return
 }
