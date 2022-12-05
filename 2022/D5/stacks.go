a934ec4755239b1ae5f361a2a559f32893153f85
Galzzly <liam.gallear@gmail.com>
2022-12-05 14:12:11 +0000

    Fixed the keys D5

--- 2022/D5/stacks.go
+++ 2022/D5/stacks.go
@@ -18,16 +18,17 @@ func main() {
 	start := time.Now()
 	f := "input.txt"
 	sects := utils.ReadFileDoubleLineNoTrim(f)
-	s1, s2 := getStack(sects[0])
+	s1, s2, k := getStack(sects[0])
 	rules := getRules(sects[1])
+	fmt.Println(k)
 
-	p1, p2 := solve(s1, s2, rules)
+	p1, p2 := solve(s1, s2, k, rules)
 	fmt.Println(p1)
 	fmt.Println(p2)
 	fmt.Println(time.Since(start))
 }
 
-func solve(s1, s2 map[rune]string, rules []rule) (r1, r2 string) {
+func solve(s1, s2 map[rune]string, k string, rules []rule) (r1, r2 string) {
 	for _, r := range rules {
 		for i := 0; i < r.num; i++ {
 			s1[r.to] += string(s1[r.from][len(s1[r.from])-1])
@@ -36,17 +37,18 @@ func solve(s1, s2 map[rune]string, rules []rule) (r1, r2 string) {
 		s2[r.to] += string(s2[r.from][len(s2[r.from])-r.num:])
 		s2[r.from] = s2[r.from][:len(s2[r.from])-r.num]
 	}
-	for i := range s1 {
+	for _, i := range k {
 		r1 += string(s1[i][len(s1[i])-1])
 		r2 += string(s2[i][len(s2[i])-1])
 	}
 	return
 }
 
-func getStack(stack string) (s1, s2 map[rune]string) {
+func getStack(stack string) (s1, s2 map[rune]string, k string) {
 	crates := strings.Split(stack, "\n")
 	keys := crates[len(crates)-1]
 	s1, s2 = map[rune]string{}, map[rune]string{}
+	k = strings.ReplaceAll(keys, " ", "")
 	for i := len(crates) - 2; i >= 0; i-- {
 		for j, c := range crates[i] {
 			if unicode.IsLetter(c) {
